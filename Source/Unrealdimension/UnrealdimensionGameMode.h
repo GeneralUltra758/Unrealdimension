fcc96553a8c49e230b2cc71756907d95ea564793
GenUltra758 <gen.ultra758@gmail.com>
2016-10-27 17:02:38 +0200

    Initial commit

---
+++ Source/Unrealdimension/UnrealdimensionGameMode.h
@@ -0,0 +1,16 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+#pragma once
+#include "GameFramework/GameMode.h"
+#include "UnrealdimensionGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AUnrealdimensionGameMode : public AGameMode
+{
+	GENERATED_BODY()
+
+public:
+	AUnrealdimensionGameMode();
+};
+
+
+
